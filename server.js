e98cd4a360b983fd7c47c9d5462c376b16190398
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-07-24 11:38:05 -0700

    fetching from datatbase to index page

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 'use strict';
 
 const express = require('express');
+const pg = require('pg');
 const superagent = require('superagent');
 const app = express();
+
+// Environment variables
+require('dotenv').config();
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,7 +19,9 @@ app.set('view engine', 'ejs'); // setting ejs as view engin for express app
 
 // API Routes
 // Renders the search form
-app.get('/', newSearch);
+
+app.get('/search', newSearch);
+app.get('/', getBooks);
 
 
 // Creates a new search to the Google Books API
@@ -24,7 +33,17 @@ app.get('*', (request, response) => response.status(404).send('This route does n
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
+
 // HELPER FUNCTIONS
+function getBooks(request, response) {
+  let SQL = 'SELECT * from books;';
+
+  return client.query(SQL)
+    .then(results => response.render('pages/index', { results: results.rows }))
+    .catch(err => response.status(500).render('pages/error'), {err: 'oops'}); 
+}
+
+
 function Book(info) {
   const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
   this.title = info.title ? info.title : 'no title';
@@ -51,8 +70,6 @@ function createSearch(request, response) {
   superagent.get(url)
     .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
     .then(results => response.render('pages/searches/show', { searchResults: results }))
-    .catch(err => handleError(err, response));
+    .catch(err => response.status(500).render('pages/error'), {err: 'oops'});
 }
-function handleError(error, response) {
-  response.render('pages/error', { error: error });
-}
+
